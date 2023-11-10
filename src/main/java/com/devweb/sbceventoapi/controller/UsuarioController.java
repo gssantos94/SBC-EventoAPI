669eddd284340c268c0dea53f9ad473a5adca74c
Gustavo Santos <gssantos@id.uff.br>
2023-11-10 20:18:55 -0300

    Criação endpoint buscar por id e listar usuários

--- src/main/java/com/devweb/sbceventoapi/controller/UsuarioController.java
+++ src/main/java/com/devweb/sbceventoapi/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package com.devweb.sbceventoapi.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +23,18 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return usuarioRepository.findAll();
+    }
+
+       @GetMapping("/{id:\\d+}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
+        return usuarioRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping
     public Usuario createUsuario(@RequestBody Usuario usuario) {

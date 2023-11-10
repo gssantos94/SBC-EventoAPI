ddddeda625b6ddacd76475120b9e83ee7571a893
Gustavo Santos <gssantos@id.uff.br>
2023-11-10 20:44:39 -0300

    Criação do Controller e Repository Patern de Edicao

---
+++ src/main/java/com/devweb/sbceventoapi/repository/EdicaoRepository.java
@@ -0,0 +1,7 @@
+package com.devweb.sbceventoapi.repository;
+
+import com.devweb.sbceventoapi.model.Edicao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EdicaoRepository extends JpaRepository<Edicao, Long> {
+}

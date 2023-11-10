b7f763cf16dc647292029facfbd52c027a569b50
Gustavo Santos <gssantos@id.uff.br>
2023-11-10 13:43:56 -0300

    Criação entidade Usuario

---
+++ src/main/java/com/devweb/sbceventoapi/model/Usuario.java
@@ -0,0 +1,109 @@
+package com.devweb.sbceventoapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String afiliacao;
+
+    
+    private boolean admin = false;
+
+    // Construtor padrão
+    public Usuario() {
+    }
+
+    // Construtor com parâmetros
+    public Usuario(String login, String email, String nome, String afiliacao, boolean admin) {
+        this.login = login;
+        this.email = email;
+        this.nome = nome;
+        this.afiliacao = afiliacao;
+        this.admin = admin;
+    }
+
+    // Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAfiliacao() {
+        return afiliacao;
+    }
+
+    public void setAfiliacao(String afiliacao) {
+        this.afiliacao = afiliacao;
+    }
+
+    public boolean isadmin() {
+        return admin;
+    }
+
+    public void setadmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id='" + id + '\'' +
+                ", login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", nome='" + nome + '\'' +
+                ", afiliacao='" + afiliacao + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
+}

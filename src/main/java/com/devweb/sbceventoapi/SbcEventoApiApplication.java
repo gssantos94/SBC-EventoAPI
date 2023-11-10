c98e71ae09b1010c3773e5b3e882f5d16c86e571
Gustavo Santos <gssantos@id.uff.br>
2023-11-10 13:43:35 -0300

    Configuração main

---
+++ src/main/java/com/devweb/sbceventoapi/SbcEventoApiApplication.java
@@ -0,0 +1,13 @@
+package com.devweb.sbceventoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcEventoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcEventoApiApplication.class, args);
+	}
+
+}

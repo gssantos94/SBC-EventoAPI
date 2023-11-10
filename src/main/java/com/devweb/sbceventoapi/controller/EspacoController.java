634f286e8ad9394671723437b68d2032796c4da5
Gustavo Santos <gssantos@id.uff.br>
2023-11-10 13:44:29 -0300

    Criação Controller e Repository Patern Espaço

---
+++ src/main/java/com/devweb/sbceventoapi/controller/EspacoController.java
@@ -0,0 +1,81 @@
+package com.devweb.sbceventoapi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devweb.sbceventoapi.model.Espaco;
+import com.devweb.sbceventoapi.repository.EspacoRepository;
+
+@RestController
+@RequestMapping("/espacos")
+public class EspacoController {
+
+    @Autowired
+    private EspacoRepository espacoRepository;
+
+    // Endpoint para listar todos os espaços
+    @GetMapping
+    public ResponseEntity<List<Espaco>> getAllEspacos() {
+        List<Espaco> espacos = espacoRepository.findAll();
+        return ResponseEntity.ok(espacos);
+    }
+
+    // Endpoint para buscar um espaço pelo ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Espaco> getEspacoById(@PathVariable Long id) {
+        Optional<Espaco> espaco = espacoRepository.findById(id);
+        if (espaco.isPresent()) {
+            return ResponseEntity.ok(espaco.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Endpoint para criar um novo espaço
+    @PostMapping
+    public ResponseEntity<Espaco> createEspaco(@RequestBody Espaco espaco) {
+        Espaco savedEspaco = espacoRepository.save(espaco);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEspaco);
+    }
+
+    // Endpoint para atualizar um espaço existente
+    @PutMapping("/{id}")
+    public ResponseEntity<Espaco> updateEspaco(@PathVariable Long id, @RequestBody Espaco espaco) {
+        Optional<Espaco> existingEspaco = espacoRepository.findById(id);
+        if (existingEspaco.isPresent()) {
+            Espaco updatedEspaco = existingEspaco.get();
+            updatedEspaco.setNome(espaco.getNome());
+            updatedEspaco.setLocalizacao(espaco.getLocalizacao());
+            updatedEspaco.setCapacidade(espaco.getCapacidade());
+            updatedEspaco.setRecursos(espaco.getRecursos());
+            espacoRepository.save(updatedEspaco);
+            return ResponseEntity.ok(updatedEspaco);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Endpoint para deletar um espaço pelo ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEspaco(@PathVariable Long id) {
+        Optional<Espaco> espaco = espacoRepository.findById(id);
+        if (espaco.isPresent()) {
+            espacoRepository.delete(espaco.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
